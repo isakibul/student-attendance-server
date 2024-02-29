6d5ec97f67c6929ef3662a4656317e5d4aeca761
isakibul <isakibul108@gmail.com>
2024-03-01 02:36:16 +0600

    file separated

--- server.js
+++ server.js
@@ -1,68 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const bcrypt = require("bcryptjs");
-var jwt = require("jsonwebtoken");
 const connectDatabase = require("./db");
-const User = require("./models/User");
 const authenticate = require("./middleware/authenticate");
+const routes = require("./routes");
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
-
-app.post("/register", async (req, res, next) => {
-  const { name, email, password } = req.body;
-
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Invalid credentials!" });
-  }
-
-  try {
-    let user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({ message: "User already exists!" });
-    }
-
-    user = new User({ name, email, password });
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    user.password = hash;
-
-    await user.save();
-
-    return res.status(201).json({ message: "User created successfully", user });
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      res.status(400).json({ message: "Invalid credential!" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credential!" });
-    }
-
-    delete user._doc.password;
-
-    const token = jwt.sign(user._doc, "secret-key", { expiresIn: "1h" });
-    return res.status(200).json({ message: "Login successful!", token });
-  } catch (e) {
-    next(e);
-  }
-});
+app.use(routes);
 
 app.get("/private", authenticate, (req, res) => {
   console.log(req.user);
@@ -70,8 +16,9 @@ app.get("/private", authenticate, (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ message: "Server error occurred" });
+  const status = err.status ? err.status : 500;
+  const message = err.message ? err.message : "Server error occurred";
+  res.status(status).json({ message });
 });
 
 const dbConnectionString = process.env.MONGODB_URI;
